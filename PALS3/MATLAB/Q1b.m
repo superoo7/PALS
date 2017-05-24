9aa2e487c1c31803017fed0feb6f27f047ed8d0b
Johnson Lai <jlwhoo7@gmail.com>
2017-05-24 12:22:22 +0800

    improved Q1b.m

--- PALS3/MATLAB/Q1b.m
+++ PALS3/MATLAB/Q1b.m
@@ -2,8 +2,8 @@
 birthYear = input('Please enter your birth year: ');
 
 while (birthYear ~= 0)	% when Y does not equal to zero
-    birthYear = input('Please enter your birth year: ');
     ageInDouble = 2017 - birthYear;
     age = num2str(ageInDouble);
     disp(['Your age is ', age])
+    birthYear = input('Please enter your birth year: ');
 end
